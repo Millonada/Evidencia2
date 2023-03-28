041a9a158e2635fefaa41ebf4f25eb70ea6b0eab
ElizaOwO <al02983142@tecmilenio.mx>
2023-03-28 16:38:21 -0600

    Inicio Evidencia2

---
+++ database/migrations/2023_03_28_220756_create_pedidos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id('id');
+            $table->foreignId('idProducto')->references('id')->on('Productos')->onDelete('cascade');
+            $table->integer('cantidad');
+            $table->float('precio',8,2);
+            $table->float('total',8,2);
+            $table->string('estatus',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
